d76aa5ff365ddb99b6d60bb0fb9f47bab82376ae
Yevhenii Vorobei <91190880+e-saprrow-life@users.noreply.github.com>
2025-06-25 13:51:35 +0300

    Fix mp4 & webm converter

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,13 @@ import newer from 'gulp-newer';
 import image from 'gulp-image';
 import convert from'heic-convert';
 import webpConverter from 'gulp-webp';
-import ffmpeg from 'fluent-ffmpeg';
 import renderSvg from "lottie-to-svg";
-var command = ffmpeg();
+
+import ffmpeg from 'fluent-ffmpeg';
+import ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+// var command = ffmpeg();
 
 
 const root = {
@@ -95,24 +99,27 @@ const to_mp4 = async () => {
             ffmpeg(path.join(root.src, file))
                 .outputOptions([
                     // https://gist.github.com/tayvano/6e2d456a9897f55025e25035478a3a50
+                    // Common
                     '-preset medium', // Впливає на швидкість і якість конвертації - ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
-                    '-crf 28', // якість відео (зазвичай для кодека libx264) Діапазон: 0 (максимальна якість, без втрат) до 51 (мінімальна якість). Рекомендоване значення: 18-28.
-                    '-c:v libx264', //  Відеокодек - Приклади: libx264, libx265, vp9.
-                    // '-c:v libvpx', //  Кодек для WebM
-                    '-c:a aac', // Аудіокодек - Приклади: aac, mp3, libopus.
-                    // '-c:a libvorbis', // Аудіокодек для WebM
-                    '-b:a 96k', // Аудіобітрейт - Приклад: -b:a 128k (128 кбіт/с)
-                    '-ar 44100', // Частота дискретизації аудіо. Приклади: 44100, 48000.
-                    // '-s <size>', // Встановлення розміру відео (ширинаxвисота) Приклад: -s 1920x1080 (Full HD).
-                    // '-aspect <ratio>', // Співвідношення сторін - Приклад: -aspect 16:9.
-                    // '-r <rate>', // Частота кадрів: Приклад: -r 30 (30 кадрів/сек).
-                    // '-vf <filter>', // Застосування відеофільтрів - Приклад: -vf "scale=1280:720" (масштабування до 720p).
-                    // '-af <filter>', // Аудіофільтри - Приклад: -af "volume=2.0" (подвоєння гучності).
                     // '-f mp4', // Формат вихідного файлу - Приклади: mp4, mkv, avi
+
+                    // Video
+                    '-crf 28',         // якість відео (зазвичай для кодека libx264) Діапазон: 0 (максимальна якість, без втрат) до 51 (мінімальна якість). Рекомендоване значення: 18-28.
+                    '-c:v libx264',    //  Відеокодек - Приклади: libx264, libx265, vp9.
+                    '-s 1920x1080',    // Встановлення розміру відео (ширинаxвисота) Приклад: -s 1920x1080 (Full HD).
+                    // '-aspect <ratio>', // Співвідношення сторін - Приклад: -aspect 16:9.
+                    // '-r <rate>',       // Частота кадрів: Приклад: -r 30 (30 кадрів/сек).
+                    // '-vf <filter>',    // Застосування відеофільтрів - Приклад: -vf "scale=1280:720" (масштабування до 720p).
+
+                    // Audio
+                    '-an', // Вимкнути аудіо
+                    // '-c:a aac',       // Аудіокодек - Приклади: aac, mp3, libopus.
+                    // '-b:a 96k',       // Аудіобітрейт - Приклад: -b:a 128k (128 кбіт/с)
+                    // '-ar 44100',      // Частота дискретизації аудіо. Приклади: 44100, 48000.
+                    // '-af <filter>',   // Аудіофільтри - Приклад: -af "volume=2.0" (подвоєння гучності).
                 ])
                 .output(outputPath)
                 .on('start', () => {
-                    // console.log('===========================')
                     console.log(`[${getCurrentTime()}] \x1b[32m=> Starting: ${file}\x1b[0m`);
                 })
                 .on('end', async  () => {
@@ -126,7 +133,6 @@ const to_mp4 = async () => {
                     console.log(`[${getCurrentTime()}] Initial size: ` + Math.floor(initialSize / 1024 / 1024) + 'Mb')
                     console.log(`[${getCurrentTime()}] Final size: ` + Math.floor(finalSize / 1024 / 1024) + 'Mb')
                     
-                    // console.log('===========================')
                     resolve(); // Завершення обіцянки
                 })
                 .on('error', (err) => {
@@ -142,6 +148,68 @@ const to_mp4 = async () => {
     }
 }
 
+
+const to_webm = async () => {
+    const source = await fs.readdir(root.src);
+    const files = source.filter(file => ['.mp4', '.mov'].includes(path.extname(file).toLowerCase()));
+
+    const processFile = async (file) => {
+        const inputPath = path.join(root.src, file);
+        const basename = path.basename(file, path.extname(file));
+        const outputPath = path.join(root.output, basename + '.webm');
+        const initialSize = await getFileSize(inputPath);
+
+        return new Promise((resolve, reject) => {
+            ffmpeg(path.join(root.src, file))                
+                .outputOptions([
+                    // https://gist.github.com/tayvano/6e2d456a9897f55025e25035478a3a50
+
+                    // Common
+                    '-preset medium',   // Баланс між швидкістю і якістю [ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow]
+                    '-f webm',          // Формат WebM
+                    '-deadline good',   // Баланс швидкість/якість [best, good]
+
+                    // Video
+                    '-c:v libvpx-vp9',  // Кодек VP9 для WebM [libvpx, libvpx-vp9]
+                    '-crf 10',          // Оптимальна якість, регулюється 0-63 (менше = краща якість)
+                    '-b:v 1M',          // Бітрейт відео 1Мбіт/с
+                    '-s 1920x1080',     // Розмір відео
+
+                    // Audio
+                    '-c:a libvorbis',   // Кращий аудіокодек Opus
+                    '-b:a 96k',         // Бітрейт аудіо
+                    '-ar 44100',        // Частота дискретизації аудіо
+                    
+                    
+                ])
+                .output(outputPath)
+                .on('start', () => {
+                    console.log(`[${getCurrentTime()}] \x1b[32m=> Starting: ${file}\x1b[0m`);
+                })
+                .on('end', async  () => {
+                    const finalSize = await getFileSize(outputPath);
+                    const diff = finalSize - initialSize; 
+                    if (diff > 0) {
+                        console.log(`[${getCurrentTime()}] Converted: \x1b[31m+` + Math.floor(diff / 1024 / 1024) + 'Mb\x1b[0m')
+                    } else {
+                        console.log(`[${getCurrentTime()}] Converted: \x1b[32m` + Math.floor(diff / 1024 / 1024) + 'Mb\x1b[0m')
+                    }
+                    console.log(`[${getCurrentTime()}] Initial size: ` + Math.floor(initialSize / 1024 / 1024) + 'Mb')
+                    console.log(`[${getCurrentTime()}] Final size: ` + Math.floor(finalSize / 1024 / 1024) + 'Mb')
+                    resolve();
+                })
+                .on('error', (err) => {
+                    console.error(`[${getCurrentTime()}] Error processing ${file}: ${err.message}`);
+                    reject(err);
+                })
+                .run(); 
+        });
+    };
+
+    for (const file of files) {
+        await processFile(file); // Чекаємо завершення попередньої операції
+    }
+}
 
 const lottie_to_svg = async () => {
     const animationData = JSON.parse(fs_native.readFileSync(root.src + '/anim.json', "utf8"));
@@ -166,81 +234,6 @@ const lottie_to_svg = async () => {
     //     processFile(file);
     // }
 }
-
-
-const to_webm = async () => {
-    const source = await fs.readdir(root.src);
-    const files = source.filter(file => ['.mp4', '.mov'].includes(path.extname(file).toLowerCase()));
-
-    const processFile = async (file) => {
-        const inputPath = path.join(root.src, file);
-        const basename = path.basename(file, path.extname(file));
-        const outputPath = path.join(root.output, basename + '.webm');
-        const initialSize = await getFileSize(inputPath);
-
-        return new Promise((resolve, reject) => {
-            ffmpeg(path.join(root.src, file))
-                // .outputOptions([
-                //     // https://gist.github.com/tayvano/6e2d456a9897f55025e25035478a3a50
-                //     '-preset medium', // Впливає на швидкість і якість конвертації - ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
-                //     '-crf 30', // якість відео (зазвичай для кодека libx264) Діапазон: 0 (максимальна якість, без втрат) до 51 (мінімальна якість). Рекомендоване значення: 18-28.
-                //     // '-c:v libvpx', //  Кодек для WebM
-                //     '-c:v libvpx-vp9', //  Кодек для WebM
-                //     '-c:a libvorbis', // Аудіокодек для WebM
-                //     '-b:v 1M',
-                //     '-deadline good', // -deadline best - Контролює компроміс між швидкістю та якістю.
-                //     '-b:a 96k', // Аудіобітрейт - Приклад: -b:a 128k (128 кбіт/с)
-                //     '-ar 44100', // Частота дискретизації аудіо. Приклади: 44100, 48000.
-                //     // '-s <size>', // Встановлення розміру відео (ширинаxвисота) Приклад: -s 1920x1080 (Full HD).
-                //     // '-aspect <ratio>', // Співвідношення сторін - Приклад: -aspect 16:9.
-                //     // '-r <rate>', // Частота кадрів: Приклад: -r 30 (30 кадрів/сек).
-                //     // '-vf <filter>', // Застосування відеофільтрів - Приклад: -vf "scale=1280:720" (масштабування до 720p).
-                //     // '-af <filter>', // Аудіофільтри - Приклад: -af "volume=2.0" (подвоєння гучності).
-                //     '-f webm', // Формат вихідного файлу - Приклади: mp4, mkv, avi, webm
-                // ])
-                
-                .outputOptions([
-                    '-preset medium',   // Баланс між швидкістю і якістю [ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow]
-                    '-c:v libvpx-vp9',  // Кодек VP9 для WebM [libvpx, libvpx-vp9]
-                    // '-c:v libvpx',  // Кодек VP9 для WebM [libvpx, libvpx-vp9]
-                    '-crf 10',          // Оптимальна якість, регулюється 0-63 (менше = краща якість)
-                    '-b:v 1M',          // Бітрейт відео 1Мбіт/с
-                    '-c:a libvorbis',     // Кращий аудіокодек Opus
-                    // '-c:a libopus',     // Кращий аудіокодек Opus
-                    '-b:a 96k',         // Бітрейт аудіо
-                    '-ar 44100',        // Частота дискретизації аудіо
-                    '-deadline good',   // Баланс швидкість/якість [best, good]
-                    '-f webm',          // Формат WebM
-                ])
-                .output(outputPath)
-                .on('start', () => {
-                    console.log(`[${getCurrentTime()}] \x1b[32m=> Starting: ${file}\x1b[0m`);
-                })
-                .on('end', async  () => {
-                    const finalSize = await getFileSize(outputPath);
-                    const diff = finalSize - initialSize; 
-                    if (diff > 0) {
-                        console.log(`[${getCurrentTime()}] Converted: \x1b[31m+` + Math.floor(diff / 1024 / 1024) + 'Mb\x1b[0m')
-                    } else {
-                        console.log(`[${getCurrentTime()}] Converted: \x1b[32m` + Math.floor(diff / 1024 / 1024) + 'Mb\x1b[0m')
-                    }
-                    console.log(`[${getCurrentTime()}] Initial size: ` + Math.floor(initialSize / 1024 / 1024) + 'Mb')
-                    console.log(`[${getCurrentTime()}] Final size: ` + Math.floor(finalSize / 1024 / 1024) + 'Mb')
-                    resolve(); // Завершення обіцянки
-                })
-                .on('error', (err) => {
-                    console.error(`[${getCurrentTime()}] Error processing ${file}: ${err.message}`);
-                    reject(err); // Відхилення обіцянки при помилці
-                })
-                .run(); // Запуск ffmpeg
-        });
-    };
-
-    for (const file of files) {
-        await processFile(file); // Чекаємо завершення попередньої операції
-    }
-}
-
 
 const getFileSize = async (filePath) => {
     try {
